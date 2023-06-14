cbcccc9033186fdabdc8acbe2e1dee8100ddb14d
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2023-06-13 22:09:59 -0300

    alterei o teste

--- teste.c
+++ teste.c
@@ -7,7 +7,7 @@ int main(){
     for (int i = 0; i < 10; i++){
         for (int j = 0; j < 10; j++){
             vetor[i][j] = 1;
-            if (i==j){
+            if (i==(j+1)){
                 vetor[i][j] = 0;
             }
         }
