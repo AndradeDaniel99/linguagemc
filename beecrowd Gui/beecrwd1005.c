610b3217099663ce1cb0ecbdd0b8e7e0053ec1ff
Guilherme Ferreira <guiguiferreiraw@gmail.com>
2023-06-23 09:53:41 -0300

    beecrwd1005

---
+++ beecrowd Gui/beecrwd1005.c
@@ -0,0 +1,14 @@
+#import <stdio.h>
+
+//Como fazer a média de duas notas
+
+int main(){
+
+    double a, b, media;
+
+    scanf("%lf\n%lf", &a, &b);
+    media = (a*3.5 + b*7.5) / (3.5 + 7.5);
+    printf("MEDIA = %.5lf\n", media);
+
+    return 0;
+}
