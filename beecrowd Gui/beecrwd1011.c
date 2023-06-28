aabd23dcb2c713f3a3d1f5dc7eb31667c72b7644
Guilherme Ferreira <guiguiferreiraw@gmail.com>
2023-06-27 21:53:48 -0300

    beecrowd 1011 e 1012

---
+++ beecrowd Gui/beecrwd1011.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  int raio;
+  double pi = 3.14159;
+  double volume;
+  
+  scanf("%d", &raio);
+  
+  volume = (4.0/3) * pi * raio * (raio * raio);
+  printf("VOLUME = %.3lf\n", volume);
+  
+    return 0;
+}
